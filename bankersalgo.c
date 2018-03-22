dcfaac0d3c04ff2f5e086e2b2bca3c434c5d9a71
Utsab06 <utsab.khakurel@bison.howard.edu>
2018-03-22 18:36:20 -0400

    Test bugs

--- bankersalgo.c
+++ bankersalgo.c
@@ -11,7 +11,10 @@ int maximum[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 
-pthread_t* threads;
+pthread_t threads[NUMBER_OF_CUSTOMERS];
+pthread_mutex_t mutex;
+
+void* runner(void* id);
 
 int main(int argc, char *argv[]) {
 	int i;
@@ -24,8 +27,10 @@ int main(int argc, char *argv[]) {
     else {
     	for (i=0;i<NUMBER_OF_RESOURCES;i++) {
     		available[i] = atoi(argv[i+1]);
-        }
-    }	
+        }	
+    }
+
+    pthread_mutex_init(&mutex, NULL);
 
     srand(time(NULL));
     for (i=0;i<NUMBER_OF_CUSTOMERS;i++) {
@@ -34,8 +39,39 @@ int main(int argc, char *argv[]) {
             maximum[i][j] = t;
             allocation[i][j] = 0;
             need[i][j] = maximum[i][j] - allocation[i][j];
-            printf("%d \n", maximum[i][j], allocation[i][j], need[i][j]);
+            // printf("%d \n", maximum[i][j]);
+            // printf("%d \n", allocation[i][j]);
+            // printf("%d \n", need[i][j]);
+
         }
     }
+
+
+    i = 0;
+    for (i;i<NUMBER_OF_CUSTOMERS;i++) {
+        int *copy = (int *) malloc(sizeof(*copy));
+        *copy = i;
+        pthread_create(&threads[i], NULL, runner, (void*) copy);
+    }
+
+
+
     return 0;
+}
+
+void *runner(void *id) {
+
+    int param = *((int *) id);
+    srand(time(NULL)); 
+
+    pthread_mutex_lock(&mutex);
+
+    printf("\n customer= %d \n", param);
+    int i;
+    for (i=0;i<NUMBER_OF_RESOURCES;i++) {
+        printf("%d \n", need[param][i]);
+    }
+
+    pthread_mutex_unlock(&mutex);
+    free(id);
 }
