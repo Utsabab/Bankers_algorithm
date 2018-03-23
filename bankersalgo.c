ccb77d142fa3deb45a89c5ca2ba85e09a56b8edc
Utsab06 <utsab.khakurel@bison.howard.edu>
2018-03-22 20:22:36 -0400

    functions added

--- bankersalgo.c
+++ bankersalgo.c
@@ -11,10 +11,11 @@ int maximum[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 
-pthread_t threads[NUMBER_OF_CUSTOMERS];
+pthread_t* threads;
+int* threadid;
 pthread_mutex_t mutex;
 
-void* runner(void* id);
+void* customer(void* id);
 
 int main(int argc, char *argv[]) {
 	int i;
@@ -23,7 +24,6 @@ int main(int argc, char *argv[]) {
         printf("You did not feed me arguments, I will die now :( ...");
         exit(1);
     }  
-
     else {
     	for (i=0;i<NUMBER_OF_RESOURCES;i++) {
     		available[i] = atoi(argv[i+1]);
@@ -31,6 +31,8 @@ int main(int argc, char *argv[]) {
     }
 
     pthread_mutex_init(&mutex, NULL);
+    threads = (pthread_t *) malloc(NUMBER_OF_CUSTOMERS * sizeof(pthread_t));
+    threadid = (int *) malloc(NUMBER_OF_CUSTOMERS * sizeof(int));
 
     srand(time(NULL));
     for (i=0;i<NUMBER_OF_CUSTOMERS;i++) {
@@ -39,28 +41,24 @@ int main(int argc, char *argv[]) {
             maximum[i][j] = t;
             allocation[i][j] = 0;
             need[i][j] = maximum[i][j] - allocation[i][j];
-            // printf("%d \n", maximum[i][j]);
-            // printf("%d \n", allocation[i][j]);
-            // printf("%d \n", need[i][j]);
-
         }
     }
 
 
-    i = 0;
-    for (i;i<NUMBER_OF_CUSTOMERS;i++) {
-        int *copy = (int *) malloc(sizeof(*copy));
-        *copy = i;
-        pthread_create(&threads[i], NULL, runner, (void*) copy);
+    for (i=0;i<NUMBER_OF_CUSTOMERS;i++) {
+        threadid[i] = i;
+        pthread_create(&threads[i], NULL, customer, (void *) (&threadid[i]));
     }
 
+    for (i=0;i<NUMBER_OF_CUSTOMERS;i++) {
+        pthread_join(threads[i], NULL);
+    }
 
 
     return 0;
 }
 
-void *runner(void *id) {
-
+void *customer(void *id) {
     int param = *((int *) id);
     srand(time(NULL)); 
 
@@ -68,10 +66,50 @@ void *runner(void *id) {
 
     printf("\n customer= %d \n", param);
     int i;
+    // for (i=0;i<NUMBER_OF_RESOURCES;i++) {
+    //     printf("%d \n", need[param][i]);
+    // }
+
+    int request[NUMBER_OF_RESOURCES];
     for (i=0;i<NUMBER_OF_RESOURCES;i++) {
-        printf("%d \n", need[param][i]);
+        request[i] = rand() % (need[param][i] + 1);
     }
+   
 
     pthread_mutex_unlock(&mutex);
-    free(id);
+}
+
+int is_request_approved(int request[] ,int id) {
+    int i;
+    for (i=0;i<NUMBER_OF_RESOURCES;i++) {
+        if (available[i] < request[i]) {
+            return 1;
+        }
+    }
+    for (i=0;i<NUMBER_OF_RESOURCES;i++) {
+        available[i] = available[i] - request[i];
+        allocation[id][i] = allocation[id][i] + request[i];
+        need[id][i] = need[id][i] - request[i];
+    }
+
+    if (is_request_safe == 0) {
+
+    }
+}
+
+int is_request_safe() {
+    int i;
+    int j;
+    for (i=0;i<NUMBER_OF_CUSTOMERS;i++) {
+        int count = 0;
+        for (j=0;j<NUMBER_OF_RESOURCES;j++) {
+            if (need[i][j] <= available[j] ) {
+                count = count + 1;
+            }
+        }
+        if (count == NUMBER_OF_RESOURCES) {
+            return 0;
+        }
+    }
+    return 1;
 }
